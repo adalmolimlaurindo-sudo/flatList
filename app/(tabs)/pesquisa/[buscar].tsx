ccd0857fe822dba29e9af376a4e841d293be57db
Nalu <adalmolimlaurindo@gmail.com>
2026-09-17 16:48:51 -0300

    oi3

--- app/(tabs)/pesquisa/[buscar].tsx
+++ app/(tabs)/pesquisa/[buscar].tsx
@@ -1,5 +1,5 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, } from "react-native";
+import { View } from "react-native";
 
 export default function pesquisa() {
     const { buscar } = useLocalSearchParams();
